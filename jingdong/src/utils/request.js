9c80c44efdf74d714287f10e4375e1e2fc444603
liujingmin <liujingmin@liujingmindeMacBook-Pro.local>
2021-07-22 15:14:43 +0800

    fix

---
+++ jingdong/src/utils/request.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+export const post = (url, data = {}) => {
+  return new Promise((resolve, reject) => {
+    axios.post(url, data, {
+      baseURL: 'https://www.fastmock.site/mock/72729968d4bb8fb9f2e9e57ee3f7a385/jdtest',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((response) => {
+      resolve(response.data)
+    }, err => {
+      reject(err)
+    })
+  })
+}
