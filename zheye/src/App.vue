88f52cf91d896991f81276a571652154e7b617fc
liujingmin <liujingmin@liujingmindeMacBook-Pro.local>
2021-08-12 17:02:43 +0800

    fix

---
+++ zheye/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+    <colum-list :list="testData"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumList, { ColumProps } from './components/ColumList.vue'
+
+const testData:ColumProps[] = [
+  {
+    id: 1,
+    title: 'test1的专栏',
+    description: '这是test1专栏，有一段非常有意思的简介，可以更新一下哦',
+    avatar: ''
+  },
+  {
+    id: 2,
+    title: 'test2的专栏',
+    description: '这是test2专栏，有一段非常有意思的简介，可以更新一下哦',
+    avatar: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
+  },
+  {
+    id: 3,
+    title: 'test3的专栏',
+    description: '这是test3专栏，有一段非常有意思的简介，可以更新一下哦',
+    avatar: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
+  },
+  {
+    id: 4,
+    title: 'test4的专栏',
+    description: '这是test4专栏，有一段非常有意思的简介，可以更新一下哦',
+    avatar: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
+  }
+]
+export default defineComponent({
+  name: 'App',
+  components: { ColumList },
+  setup () {
+    return { testData }
+  }
+})
+</script>
+
+<style>
+</style>
---
+++ zheye/src/components/ColumList.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div class="row">
+      <div v-for="column in columnList" :key="column.id" class="col-4 mb-4">
+        <div class="card h-100 shadow-sm">
+          <div class="card-body text-center">
+            <img :src="column.avatar" class="rounded-circle border border-light w-25 my-3" :alt="column.title">
+            <h5 class="card-title">{{column.title}}</h5>
+            <p class="card-text text-left">{{column.description}}</p>
+            <a href="#" class="btn btn-outline-primary">进入专栏</a>
+          </div>
+      </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType, computed } from 'vue'
+
+export interface ColumProps {
+  id: number,
+  title: string,
+  avatar?: string,
+  description: string
+}
+
+const useComputedColumnlist = (props) => {
+  const columnList = computed(() => {
+    const result = props.list.map(item => {
+      if (!item.avatar) {
+        item.avatar = require('@/assets/column.jpg')
+      }
+      return item
+    })
+    return result
+  })
+  return { columnList }
+}
+export default defineComponent({
+  name: 'ColumList',
+  props: {
+    list: {
+      type: Array as PropType<ColumProps[]>,
+      required: true
+    }
+  },
+  setup (props) {
+    const { columnList } = useComputedColumnlist(props)
+    return { columnList }
+  }
+})
+</script>
