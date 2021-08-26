ebedf347240f7719e7f3ff31517113ed472565a2
liujingmin <liujingmin@liujingmindeMacBook-Pro.local>
2021-08-26 09:55:53 +0800

    fix

--- typescript-test/dist/src.f10117fe.js
+++ typescript-test/dist/src.f10117fe.js
@@ -151,7 +151,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60379" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61322" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
