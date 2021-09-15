ab9cb7e69a02e04b1993276dd90d368ccc5d34a3
liujingmin <liujingmin@liujingmindeMacBook-Pro.local>
2021-09-15 18:02:47 +0800

    fix

--- es-demo/src/2-11.js
+++ es-demo/src/2-11.js
@@ -1,11 +1,18 @@
 function People(name, age) {
-  console.log(this)
+  console.log(this) // this 指向实例对象本身
   this.name = name
   this.age = age
+  People.count++
 }
 
 People.prototype.showName = function() {
   console.log('我的名字是', this.name)
+}
+
+People.count = 0
+People.showCount = function(){
+  console.log(this) // this 指向的是构造函数本身 所以这里调用不到this.name this.age
+  console.log('调用的个数是', People.count)
 }
 
 let p1 = new People('mm', 33)
@@ -13,3 +20,7 @@ p1.showName()
 
 let p2 = new People('zhangsan', 22)
 p2.showName()
+
+// 显示2
+People.showCount()
+
